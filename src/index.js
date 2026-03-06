305b678d6961d2b885e0b436fc18e5ea5b4a956d
Srishruti24 <sri.shruti24@gmail.com>
2026-03-07 03:26:00 +0530

    Created nodemailer transport object and tried

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 const express=require('express');
-const {ServerConfig,Logger}=require('./config');
+const {ServerConfig}=require('./config');
 const apiRoutes=require('./routes');
 
+const mailsender =require('./config/email-config')
 const app=express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 app.use('/api',apiRoutes);
 
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT,async()=>{
     console.log(`Server is running on port: ${ServerConfig.PORT}`);
-    Logger.info("Successfully started the server","root",{});
+    try {
+        const response= await mailsender.sendMail({
+        from:ServerConfig.GMAIL_EMAIL,
+        to:'sri.shruti24@gmail.com',
+        subject:'Is the service working ?',
+        text:'Yes it is working'
+    });
+    console.log(response);
+    } catch (error) {
+       console.log(error); 
+    }
 });
