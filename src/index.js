255cd2c43ade5fc86e933fbccc73fed784b30f55
Srishruti24 <sri.shruti24@gmail.com>
2026-03-08 01:58:32 +0530

    Added create ticket api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express=require('express');
 const {ServerConfig}=require('./config');
 const apiRoutes=require('./routes');
 
-const mailsender =require('./config/email-config')
 const app=express();
 
 app.use(express.json());
@@ -13,15 +12,4 @@ app.use('/api',apiRoutes);
 
 app.listen(ServerConfig.PORT,async()=>{
     console.log(`Server is running on port: ${ServerConfig.PORT}`);
-    try {
-        const response= await mailsender.sendMail({
-        from:ServerConfig.GMAIL_EMAIL,
-        to:'sri.shruti24@gmail.com',
-        subject:'Is the service working ?',
-        text:'Yes it is working'
-    });
-    console.log(response);
-    } catch (error) {
-       console.log(error); 
-    }
 });
---
+++ src/utils/common/success-response.js
@@ -0,0 +1,7 @@
+const success = {
+    success:true,
+    message:'Request was successful',
+    data:{},
+    error:{}
+}
+module.exports = success;
